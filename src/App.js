6c5f152f01d0c226d92751e06439d23f1aba2dc5
dewreeh <severussenap@gmail.com>
2024-11-11 23:39:56 +0300

    added login form for users

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import  './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
 
+      <div className="App">
+        <div className="body">
+          <Header />
+        </div>
+        <p className="AdminEntryButton"> Вход для администратора </p>
+      </div>
 
   );
 }
---
+++ src/FormUser.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './index.css'
+function AdminUser(){
+
+}
+
+export default AdminUser;
