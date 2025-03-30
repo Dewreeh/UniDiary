6117a70f72f8949981cb7820e28a1dbb40e944ed
dewreeh <severussenap@gmail.com>
2025-03-30 17:01:07 +0300

    Kafka consumer for emails initialisation

---
+++ backend/src/main/java/org/repin/service/KafkaProducerService.java
@@ -0,0 +1,8 @@
+package org.repin.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducerService {
+    //Чтобы в дальнейшем отправлять уведомления на почту через отдельный микросервис
+}
